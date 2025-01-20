b6670859df2dfb3561ecb30ad3fbd55bb854a660
bruno1098 <rm98470@fiap.com.br>
2025-01-20 15:03:23 -0300

    Arrumando erro nas importaçoes dos modulos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,37 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthorListComponent } from './components/author-list/author-list.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { ChatComponent } from './components/chat/chat.components';
-import { AuthorBooksListComponent } from './components/author-book-list/author-book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorListComponent,
     BookListComponent,
-    ChatComponent,
-    AuthorBooksListComponent
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
     FormsModule,
     CommonModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { path: '', redirectTo: '/autores', pathMatch: 'full' },
       { path: 'autores', component: AuthorListComponent },
       { path: 'livros', component: BookListComponent },
-      { path: 'chat', component: ChatComponent },
-      { path: 'autores/:id/livros', component: AuthorBooksListComponent }
+      { path: 'chat', component: ChatComponent }
     ])
   ],
   providers: [],
